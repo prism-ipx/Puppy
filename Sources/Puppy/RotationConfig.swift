b240baf270110afce8f09676836b7aa4fa52067b
bruce quinton <zanoroy@gmail.com>
2023-01-19 12:20:37 +1030

    Fixed RotationalType instantiation

--- Sources/Puppy/RotationConfig.swift
+++ Sources/Puppy/RotationConfig.swift
@@ -17,9 +17,9 @@ public struct RotationConfig: Sendable {
     public var maxArchives: UInt8
 
     public init(rotationType: RotationType = .filesize, suffixExtension: SuffixExtension = .numbering, maxFileSize: ByteCount = 10 * 1024 * 1024, maxArchives: UInt8 = 5) {
+        self.rotationType = rotationType
         self.suffixExtension = suffixExtension
         self.maxFileSize = maxFileSize
         self.maxArchives = maxArchives
-        self.rotationType = .filesize
     }
 }
